cfd68fec390d0803739de6d006191615c63b4523
HaleHasdemir <halesh2@illinois.edu>
2024-06-25 09:18:14 -0700

    info.py updated

--- mdgeom/info.py
+++ mdgeom/info.py
@@ -1,13 +1,79 @@
+# -*- coding: utf-8 -*-
 """
 Information about simulations
-=========
+=============================
 
-This module contains ...
+This module contains functions that provide overview information about a
+trajectory that may be useful for a Methods section of a paper.
 
 """
+import prettytable
 
-def summary(*universe):
-    "hello world"
+
+def summary(*universes, labels=None):
+    """Print summary information about all `universes`.
+
+    Parameters
+    ----------
+    universes : Universe
+        Print information about these simulation systems
+        in a table.
+
+    labels: list of str, optional
+        Add a label to the output, e.g., the name or ID of the simulation.
+        There needs to be one element on `labels` for each universe.
+
+    """
+    data_column_keys = [
+        "n_atoms",
+        "Lx",
+        "Ly",
+        "Lz",
+        "alpha",
+        "beta",
+        "gamma",
+        "n_frames",
+        "totaltime",
+        "dt",
+    ]
+    data_column_names = [
+        "n_atoms",
+        "Lx/Å",
+        "Ly/Å",
+        "Lz/Å",
+        "alpha",
+        "beta",
+        "gamma",
+        "n_frames",
+        "totaltime/ns",
+        "dt/ps",
+    ]
+
+    if labels is None:
+        labels = len(universes) * [None]
+        column_names = data_column_names
+    else:
+        column_names = ["simulation"] + data_column_names
+
+    if len(labels) != len(universes):
+        raise ValueError(
+            f"labels {labels} must contain one "
+            f"label per universe {universes}"
+        )
+
+
+    table = prettytable.PrettyTable()
+    table.field_names = column_names
+
+    for u, label in zip(universes, labels):
+        data = extract(u)
+        data["totaltime"] /= 1000  # convert ps to ns
+        row = [data[key] for key in data_column_keys]
+        if label is not None:
+            row = [label] + row
+        table.add_row(row)
+
+    print(table)
 
 
 def extract(u):
@@ -52,4 +118,3 @@ def extract(u):
         "dt": u.trajectory.dt,
     }
     return data
-
